ec32098426d6e42b9533837e8580e7f1b2296b9d
Ved <dev@vedat.xyz>
2022-03-26 20:16:43 +0300

    Don't test 2FA when no credentials given

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,7 @@ import { getMealList } from "./cafeteria";
 import { get2FACode } from "./srs2fa";
 
 const testDir = path.join(__dirname, "..", "testoutput");
+const test2FACredentials = { email: "", password: "" };
 
 (async () => {
   Object.keys(Department).forEach((dep) =>
@@ -14,6 +15,8 @@ const testDir = path.join(__dirname, "..", "testoutput");
 
   await fs.promises.writeFile(path.join(testDir, "meallist.json"), JSON.stringify(await getMealList(), null, 2));
 
-  const { code, ref } = await get2FACode("", "", "STARS Auth");
-  console.log(`Code: ${code}, Ref: ${ref}`);
+  if (test2FACredentials.email.length > 10 && test2FACredentials.password.length > 5) {
+    const { code, ref } = await get2FACode(test2FACredentials.email, test2FACredentials.password, "STARS Auth");
+    console.log(`Code: ${code}, Ref: ${ref}`);
+  }
 })();
