09e053c9a6983c8b69dcd895f09481f8878835ff
Ved <dev@vedat.xyz>
2022-05-31 23:27:08 +0300

    Improve testing separate features

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,51 @@
 import path from "path";
 import fs from "fs";
+import promptSync from "prompt-sync";
 import { saveOfferingsToJSON } from "./offerings";
 import { Department, SemesterType } from "./offerings/interface";
 import { getMealList } from "./cafeteria";
-import { get2FACode } from "./srs2fa";
+import { get2FACode, initializeLogin, verifyEmail } from "./srs";
 
+const args = process.argv.slice(2);
 const testDir = path.join(__dirname, "..", "testoutput");
-const test2FACredentials = { email: "", password: "" };
+const prompt = promptSync({ sigint: true });
 
-(async () => {
+const testOfferings = () => {
   Object.keys(Department).forEach((dep) =>
     saveOfferingsToJSON({ department: dep as Department, year: 2021, semesterType: SemesterType.Spring }, true, testDir)
   );
+};
 
+const testCafeteria = async () => {
   await fs.promises.writeFile(path.join(testDir, "meallist.json"), JSON.stringify(await getMealList(), null, 2));
+};
 
-  if (test2FACredentials.email.length > 10 && test2FACredentials.password.length > 5) {
-    const { code, ref } = await get2FACode(test2FACredentials.email, test2FACredentials.password, "STARS Auth");
+const test2FA = async () => {
+  const email = args.find((i) => i.startsWith("--email="))?.substring("--email=".length) || "";
+  const password = args.find((i) => i.startsWith("--password="))?.substring("--password=".length) || "";
+
+  if (email.length > 10 && password.length > 5) {
+    const { code, ref } = await get2FACode(email, password, "STARS Auth");
     console.log(`Code: ${code}, Ref: ${ref}`);
   }
+};
+
+const testLogin = async () => {
+  const id = args.find((i) => i.startsWith("--id="))?.substring("--id=".length) || "";
+  const password = args.find((i) => i.startsWith("--password="))?.substring("--password=".length) || "";
+  if (id.length < 8 || password.length < 6) throw Error("Login credentials aren't provided!");
+
+  const loginRequest = await initializeLogin(id, password);
+  console.log(loginRequest);
+
+  const verificationCode = prompt("Verification Code: ");
+  const sessid = await verifyEmail(loginRequest.cookie, verificationCode);
+  console.log(sessid);
+};
+
+(async () => {
+  if (args.includes("--offerings")) testOfferings();
+  else if (args.includes("--cafeteria")) testCafeteria();
+  else if (args.includes("--2fa")) test2FA();
+  else if (args.includes("--login")) testLogin();
 })();
