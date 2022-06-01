eb7359f4fcb3ef5e006a6495da9bff8e01528896
Ved <dev@vedat.xyz>
2022-06-02 02:29:17 +0300

    Implement builder for SRS sessions

--- src/srs/session.ts
+++ src/srs/session.ts
@@ -1,10 +1,9 @@
-/* eslint-disable import/prefer-default-export */
-
+import { get2FACode, initializeLogin, verifyEmail } from "./auth";
 import { calculateGPA } from "./cgpacalculator";
 import { CGPACalculation, SRSCourse, SRSGrades, SRSSemester, SRSSemesterCourses } from "./interface";
 import { getCurrentSemester, getSemester } from "./semester";
 
-export class SRSSession {
+class SRSSessionInternal {
   readonly cookie: string;
 
   constructor(cookie: string) {
@@ -20,3 +19,39 @@ export class SRSSession {
     return calculateGPA(this.cookie, courses);
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SRSSession extends SRSSessionInternal {}
+
+export const SRSSessionBuilder = {
+  withCookie: (cookie: string): SRSSession => new SRSSessionInternal(cookie),
+  withManualVerification: async (
+    id: string,
+    password: string
+  ): Promise<{ reference: string; verify: (verificationCode: string) => Promise<SRSSession> }> => {
+    const loginRequest = await initializeLogin(id, password);
+    return {
+      reference: loginRequest.reference,
+      verify: async (verificationCode) => {
+        const cookie = await verifyEmail(loginRequest.cookie, verificationCode);
+        return new SRSSessionInternal(cookie);
+      },
+    };
+  },
+  withAutomatedVerification: async (
+    id: string,
+    password: string,
+    email: string,
+    emailPassword: string,
+    boxname = "STARS Auth"
+  ): Promise<SRSSession> => {
+    const loginRequest = await initializeLogin(id, password);
+    const verificationCode = await get2FACode(email, emailPassword, boxname);
+    if (loginRequest.reference !== verificationCode.ref)
+      throw Error(
+        `Reference code mismatch during automated verification (${loginRequest.reference} != ${verificationCode.code})`
+      );
+    const cookie = await verifyEmail(loginRequest.cookie, verificationCode.code);
+    return new SRSSessionInternal(cookie);
+  },
+};
